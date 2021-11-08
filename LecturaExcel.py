64534037ec9efbfb87ca0c2c6bd68b4ae591ff2e
Josema Saborido <49287516+jsabnor@users.noreply.github.com>
2021-11-08 15:02:59 +0100

    Create LecturaExcel.py

---
+++ LecturaExcel.py
@@ -0,0 +1,8 @@
+#Importar pandas
+import pandas as pd
+#Crea un Data Frame y asignamos la lectura del archivo excel
+df=pd.read_excel()
+#Reescribimos el Data Frame, quitando las filas que tienen el valor de la 
+#columna email duplicado
+df=df.drop_duplicates(columna="email")
+#Imprimimos la columna email
